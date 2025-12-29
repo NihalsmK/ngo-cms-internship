e42fe3e8f3ea4671396800aa055282603d2d3513
Nihal Khare <nihalsmkhare@gmail.com>
2025-12-29 14:43:00 +0530

    EMERGENCY: Fix broken imports - rollback routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import Dashboard from './components/Dashboard';
-// Role-based components (create empty ones first)
-import DonorDonate from './components/donor/DonorDonate';
-import DonorHistory from './components/donor/DonorHistory';
-import AdminUsers from './components/admin/AdminUsers';
-import AdminDonations from './components/admin/AdminDonations';
-import VolunteerTasks from './components/volunteer/VolunteerTasks';
-import VolunteerMyTasks from './components/volunteer/VolunteerMyTasks';
 
 function App() {
   return (
@@ -18,13 +11,6 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        {/* Role-based routes */}
-        <Route path="/donor/donate" element={<DonorDonate />} />
-        <Route path="/donor/history" element={<DonorHistory />} />
-        <Route path="/admin/users" element={<AdminUsers />} />
-        <Route path="/admin/donations" element={<AdminDonations />} />
-        <Route path="/volunteer/tasks" element={<VolunteerTasks />} />
-        <Route path="/volunteer/my-tasks" element={<VolunteerMyTasks />} />
         <Route path="/" element={<Login />} />
       </Routes>
     </Router>
