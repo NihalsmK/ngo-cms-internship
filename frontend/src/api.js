3962897610621136dad5ca0378b8323d86659007
Nihal Khare <nihalsmkhare@gmail.com>
2026-01-02 21:05:22 +0530

    Fix loginUser export and build error

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,28 +1,12 @@
-import axios from "axios";
+import axios from 'axios';
 
-const API_BASE = "https://ngo-cms-internship.onrender.com";
+const api = axios.create({
+  baseURL: 'http://127.0.0.1:8000/api/',
+});
 
-export const registerUser = (data) => {
-  return axios.post(
-    `${API_BASE}/api/auth/register/`,
-    data,
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
-};
+export const registerUser = (data) => api.post('auth/register/', data);
+export const loginUser = (data) => api.post('auth/login/', data);
+export const me = (token) =>
+  api.get('auth/me/', { headers: { Authorization: `Bearer ${token}` } });
 
-export const loginUser = (data) => {
-  return axios.post(
-    `${API_BASE}/api/auth/login/`,
-    data,
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
-};
-
+export default api;
